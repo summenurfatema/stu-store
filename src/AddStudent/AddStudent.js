e2f21c89cf25ef476de162a3159fe5a9d559f206
summenurfatema <shadrinmoni15@gmail.com>
2023-02-07 23:49:34 +0600

    25% complete

--- src/AddStudent/AddStudent.js
+++ src/AddStudent/AddStudent.js
@@ -33,7 +33,7 @@ function AddStudentForm() {
 
   function handleSubmit(event) {
     event.preventDefault();
-    // You can add code here to submit the form data to
+    // You can add code here to submit the form data
     const fullName =`${firstName} ${middleName} ${lastName} `;
     const classDevision =`${className}-${division}`
     const studentData = {
